48f7437dff9b143c1ad5ba14eb8c583b0f90f917
Brody Blair <brocbla@umich.edu>
2024-08-20 22:52:06 -0500

    add Icons and change stylings

--- src/lib/components/dialogs/ContactAccountManager.svelte
+++ src/lib/components/dialogs/ContactAccountManager.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import Dialog from "../ui/Dialog.svelte";
-  import Button from "../ui/Button.svelte";
-  import Header from "../ui/Header.svelte";
+  import Dialog from "$lib/components/ui/Dialog.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
   import { createEventDispatcher } from "svelte";
 
   export let name;
@@ -17,10 +16,10 @@
 </script>
 
 <Dialog class="">
-  <div class="">
+  <p>
     To update your <strong>{field}</strong> you will need to contact your account
     manager:
-  </div>
+  </p>
   <div class="mx-4 bg-accent-1 rounded shadow-md p-2">
     <div><strong>Name:</strong> {name}</div>
     <div><strong>Email:</strong> {email}</div>
@@ -29,6 +28,6 @@
   <Button
     event="closeContact"
     on:closeContact={closeContact}
-    class="place-self-end">Close</Button
+    class="place-self-stretch">Close</Button
   >
 </Dialog>
--- src/lib/components/ui/Dialog.svelte
+++ src/lib/components/ui/Dialog.svelte
@@ -3,6 +3,6 @@
   export { className as class };
 </script>
 
-<dialog open class="grid gap-2 border-2 border-solid border-primary px-4 py-2 rounded-md shadow-lg max-w-sm {className}">
+<dialog open class="grid gap-2 border-2 border-solid px-4 py-2 rounded-md shadow-lg max-w-sm {className}">
   <slot />
 </dialog>
